e295708a4f71b193d5f13b772d574ba11bbeb1b4
Alex <141815742+darwinsorchid@users.noreply.github.com>
2025-11-24 19:31:11 +0200

    Final repo cleanup

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import { NavBar } from "./components/NavBar.jsx";
+
+export const About = () => {
+  return (
+    <div className="myname">
+      <h1>ALEXANDRA BEKOU</h1>
+    </div>
+  );
+};
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+const navItems = [
+  { name: "About", href: "#about" },
+  { name: "Research", href: "#research" },
+  { name: "TechStack", href: "#techstack" },
+  { name: "LabSkills", href: "#labskills" },
+  { name: "FunProjects", href: "#funprojects" },
+  { name: "Contact", href: "#contact" },
+];
+
+export const NavBar = () => {};
