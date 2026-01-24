851cdae6f99ef68e5b02610c8945cd5920d8a8a2
Alex <141815742+darwinsorchid@users.noreply.github.com>
2026-01-24 20:50:51 +0200

    Add MagicUI SmoothCursor

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import TechStack from "./components/TechStack.jsx";
 import LabSkills from "./components/LabSkills.jsx";
 import FunProjects from "./components/FunProjects.jsx";
 import Contact from "./components/Contact.jsx";
-import { useScroll } from "framer-motion";
-import { Pointer } from "@/components/ui/pointer";
+// import { useScroll } from "framer-motion";
+// import { Pointer } from "@/components/ui/pointer";
 import ParticlesComponent from "./components/ParticleBackground.jsx";
+import { SmoothCursor } from "@/components/ui/smooth-cursor";
 
 function App() {
   return (
-    <div className="relative min-h-screen z-0 overflow-hidden">
+    <div className="relative min-h-screen z-0 overflow-hidden cursor-none">
+      <SmoothCursor />
       <ParticlesComponent />
 
       <main className="relative z-10 flex flex-col">
