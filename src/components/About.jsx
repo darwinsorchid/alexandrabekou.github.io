b817236c6acb0354811bf3091873c1bd34f5c888
Alex <141815742+darwinsorchid@users.noreply.github.com>
2026-03-15 15:59:07 +0200

    Fix about layout and content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,41 +11,51 @@ const About = () => {
       className="h-screen flex flex-col text-white px-6 w-full md:px-16 lg:px-32 justify-center"
     >
       <Navbar />
-      <div className="max-w-4xl mr-auto flex flex-col">
-        <h1 className="text-5xl md:text-5xl font-light tracking-normal uppercase leading-tight mt-20">
-          ALEXANDRA BEKOU
-        </h1>
-        {/* <h1 className="text-5xl md:text-5xl font-normal tracking-normal uppercase leading-tight mt-[-1.4rem]">
-          BEKOU
-        </h1> */}
-      </div>
 
-      <div className="mt-10 ml-130 pb-1 self-center">
-        <h2 className="mt-4 text-lg md:text-3xl tracking-wide font-semibold self-end">
-          <HyperText delay={800} characterSet="[IHAVEBEENWATCHING]">
-            INTERDISCIPLINARY RESEARCHER
-          </HyperText>
-        </h2>
+      {/* Align name with title */}
+      <div className="flex w-full items-start justify-between mt-20">
+        <div>
+          <h1 className="text-white text-5xl md:text-5xl tracking-normal uppercase leading-tight">
+            ALEXANDRA BEKOU
+          </h1>
+        </div>
 
-        <div className="border-l-1 border-purple-700 pb-1 self-center">
-          <h2 className="mt-2 pl-2 text-lg md:text-3xl text-purple-700/80 font-thin">
-            <TextAnimate animation="blurInUp" by="character" once>
-              Next-generation of biological research
-            </TextAnimate>
+        <div className="text-right mt-20">
+          <h2 className="text-lg md:text-2xl tracking-wide font-semibold self-end">
+            <HyperText delay={800} characterSet="[IHAVEBEENWATCHING]">
+              INTERDISCIPLINARY RESEARCHER
+            </HyperText>
           </h2>
+
+          <div className="border-l border-purple-700 pb-1 self-center pr-5">
+            <h2 className="mt-2 text-lg md:text-3xl text-purple-700/80 font-thin">
+              <TextAnimate animation="blurInUp" by="character" once>
+                Biological Systems and Artificial Intelligence
+              </TextAnimate>
+            </h2>
+          </div>
         </div>
       </div>
 
-      <div className="w-full max-w-6xl mr-16 md:mr-24 flex flex-col pt-35 pl-20">
+      <div className="w-full max-w-4xl mr-16 md:mr-24 flex flex-col pt-25 pl-20">
         <h2 className="text-4xl font-small text-white mb-1">
-          <span className="text-4xl">EXPERIMENTAL DESIGN</span>:
+          <span className="text-3xl">RESEARCH INTERESTS</span>:
         </h2>
         {/* <h3>
           <MorphingText texts={["Machine Learning", "Molecular Research"]} />
         </h3> */}
-        <ul className="mt-2 space-y-1 text-2xl font-light">
-          <li className="list-disc list-inside">Machine Learning</li>
-          <li className="list-disc list-inside">Molecular Research</li>
+        <ul className="mt-2 space-y-1 text-xl font-light">
+          <li className="list-disc list-inside">
+            Bio-inspired Neural Networks
+          </li>
+          <li className="list-disc list-inside">Neuroevolution</li>
+          <li className="list-disc list-inside">Computational Biology</li>
+          <li className="list-disc list-inside">
+            Organoids and Disease Modeling
+          </li>
+          <li className="list-disc list-inside">
+            Machine Learning for Biomedical Data
+          </li>
         </ul>
       </div>
     </section>
